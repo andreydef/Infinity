1b018de69364763273af283bf7e821dfed2910de
Andriy Halelyuka <43058552+andreydef@users.noreply.github.com>
2021-05-04 15:52:10 +0300

    Initial commit
    1. Create start ASP .NET Core project with Angular project;
    2. Updating the implementation of interaction between projects;
    3. Updating favicon.ico in Portfolio;
    4. Rewiev and update some files in default projects;
    5. Adaptiving project to future changes;
    6. Adding something .gitignore and .gitattributes files;

---
+++ Infinity_app/Infinity_app/Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Infinity_app
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
